946f18bc42fca4c14a21926d58f518d3047c0a67
Blanka Gubcsi <blanka.gubcsi@ugent.be>
2026-01-11 15:26:35 +0100

    tawk.to2

--- components/technicalComponents/HeadComponent/HeadComponent.js
+++ components/technicalComponents/HeadComponent/HeadComponent.js
@@ -67,7 +67,7 @@ export default class HeadComponent extends Component {
 					(function(){
 					var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 					s1.async=true;
-					s1.src='https://embed.tawk.to/6963ad8d7c8bd319796ace51/';
+					s1.src='https://embed.tawk.to/6963ad8d7c8bd319796ace51/1jemlrspv';
 					s1.charset='UTF-8';
 					s1.setAttribute('crossOrigin','*');
 					s0.parentNode.insertBefore(s1,s0);})();`,
