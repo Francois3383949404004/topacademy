7873d1f429cf9a6f8b45fcac203a07dde845915c
Blanka Gubcsi <blanka.gubcsi@ugent.be>
2026-01-12 18:02:53 +0100

    test

--- components/layoutComponents/TwoCol/TwoCol.js
+++ components/layoutComponents/TwoCol/TwoCol.js
@@ -1,27 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 import css from "./TwoCol.module.scss";
 
-export default class TwoCol extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<>
-				<section {...storyblokEditable(this.props.blok)} className={css["cols"]}>
-					<div className={css["colone"]}>
-						{this.props.blok.colone.map((nestedBlok) => (
-							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-						))}
-					</div>
-					<div className={css["coltwo"]}>
-						{this.props.blok.coltwo.map((nestedBlok) => (
-							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-						))}
-					</div>
-				</section>
-			</>
-		);
-	}
-}
+const TwoCol = ({ blok }) => {
+  return (
+    <section className={css.wrapper} {...storyblokEditable(blok)}>
+      <div className={css.container}>
+        
+        {/* Column One */}
+        <div className={css.col}>
+          {blok.colone.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
+        </div>
+
+        {/* Column Two */}
+        <div className={css.col}>
+          {blok.coltwo.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
+        </div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default TwoCol;
